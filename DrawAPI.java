30542043c6d65046a1528329ccd0b0f87a7040a7
2200031989 <140400330+2200031989@users.noreply.github.com>
2023-07-29 14:05:53 +0530

    Program Bridge Pattern(27/07/23

---
+++ DrawAPI.java
@@ -0,0 +1,8 @@
+package com.sreedhar;
+
+//Bridge implementer: DrawAPI
+
+public interface DrawAPI 
+{
+	void drawCircle(int radius, int x, int y);
+}
---
+++ GreenCircle.java
@@ -0,0 +1,9 @@
+package com.sreedhar;
+
+public class GreenCircle implements DrawAPI
+{
+	public void drawCircle(int radius, int x,int y)
+	{
+		System.out.println("Drawing green circle with radius "+radius+" at ("+x+ ", "+y+ ")");
+	}
+}
